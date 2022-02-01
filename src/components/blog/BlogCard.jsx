0d144716f1eb897bc066ff9045df112eef41e718
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-02-01 14:33:50 +0530

    animation fix

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function BlogCard({blog}) {
-  return <>
-        <div className=''>
-            <img src={blog.image} alt="blog" className='w-full'/>
+  return <div className='image-scale-wrapper'>
+        <div className='overflow-hidden'>
+            <img src={blog.image} alt="blog" className='w-full image-scale'/>
         </div>
         <div className='w-10/12 px-8 py-12 ml-auto -mt-8 relative z-[2] text-left bg-white'>
             <p className='text-xs text-gray/60'>ADMIN &nbsp; {blog.date}</p>
             <h3 className='my-4 text-lg font-semibold font-playfair'>{blog.title}</h3>
             <Link to='' className="mt-2 border-none el__hover btn btn-secondary-light">Read More</Link>
         </div>
-  </>;
+  </div>;
 }
 
 export default BlogCard;
--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -18,10 +18,10 @@ function PortfolioCard({data, id}) {
     }, []);
     
 
-  return <div className={`mt-8  portfolio__image__tl${id}`}>
+  return <div className={`mt-8 image-scale-wrapper portfolio__image__tl${id}`}>
 
-      <div className={`relative portfolio__image__wrapper`}>
-          <img src={data.image} alt={data.title} className={`object-cover w-full h-full`}/>
+      <div className={`relative portfolio__image__wrapper overflow-hidden`}>
+          <img src={data.image} alt={data.title} className={`object-cover w-full h-full image-scale`}/>
           <div className='absolute top-0 left-0 w-full h-full transition-all duration-300 bg-black/0 portfolio__image__overlay z-[1]'></div>
       </div>
       <div className='p-3 mt-3 text-left porfolio__text__wrapper'>
